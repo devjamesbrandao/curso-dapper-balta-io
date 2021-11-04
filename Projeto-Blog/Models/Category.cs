46afbe1d127e25de895cbf9e04567a4a09f7b780
James Noleto Brandao da Silva <muricijames@gmail.com>
2021-11-03 22:44:39 -0300

    Projeto Blog com Dapper

---
+++ Projeto-Blog/Models/Category.cs
@@ -0,0 +1,12 @@
+using Dapper.Contrib.Extensions;
+
+namespace Models
+{
+    [Table("[Category]")]
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+    }
+}
