0bad752679085afd06cc5b851b4bc0d9c7df9aa3
James Noleto Brandao da Silva <muricijames@gmail.com>
2021-11-03 22:28:39 -0300

    Métodos básicos de acesso a dados com Dapper

---
+++ Dapper-basico/Models/Category.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Dapper_basico.Models
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Summary { get; set; }
+        public int Order { get; set; }
+        public string Description { get; set; }
+        public bool Featured { get; set; }
+    }
+}
